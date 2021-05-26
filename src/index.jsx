9314af0380cedb2abde58315b8d34bd238c2414d
Alan Kuriakose <aln0071@gmail.com>
2021-05-26 19:40:34 +0530

    Remove demo app and polyfill

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
-import 'react-rte/lib/demo';
 
 ReactDOM.render(
   <Provider store={store}>
