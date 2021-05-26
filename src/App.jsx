f8be0e5cfa7c23b7e6aeb82032fab26671984cc6
Alan Kuriakose <aln0071@gmail.com>
2021-05-26 21:17:45 +0530

    Configure buttons and their order in rte toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import RichTextEditor, {
   getTextAlignClassName,
 } from 'react-rte/lib/RichTextEditor';
 
+const rteToolbarConfig = {
+  display: [
+    'HISTORY_BUTTONS',
+    'BLOCK_TYPE_DROPDOWN',
+    'INLINE_STYLE_BUTTONS',
+    'BLOCK_ALIGNMENT_BUTTONS',
+    'BLOCK_TYPE_BUTTONS',
+  ],
+  INLINE_STYLE_BUTTONS: [
+    { label: 'Bold', style: 'BOLD', className: 'custom-css-class' },
+    { label: 'Italic', style: 'ITALIC' },
+    { label: 'Underline', style: 'UNDERLINE' },
+  ],
+  BLOCK_TYPE_DROPDOWN: [
+    { label: 'Paragraph', style: 'unstyled' },
+    { label: 'Heading Large', style: 'header-one' },
+    { label: 'Heading Medium', style: 'header-two' },
+    { label: 'Heading Small', style: 'header-three' },
+  ],
+  BLOCK_TYPE_BUTTONS: [
+    { label: 'UL', style: 'unordered-list-item' },
+    { label: 'OL', style: 'ordered-list-item' },
+  ],
+  BLOCK_ALIGNMENT_BUTTONS: [
+    { label: 'Align Left', style: 'ALIGN_LEFT' },
+    { label: 'Align Center', style: 'ALIGN_CENTER' },
+    { label: 'Align Right', style: 'ALIGN_RIGHT' },
+    { label: 'Align Justify', style: 'ALIGN_JUSTIFY' },
+  ],
+};
+
 function App() {
   const [textValue, setTextValue] = useState(createEmptyValue());
   return (
@@ -13,8 +44,9 @@ function App() {
       <div>
         <RichTextEditor
           value={textValue}
-          onChange={(value) => setTextValue(value)}
+          onChange={setTextValue}
           blockStyleFn={getTextAlignClassName}
+          toolbarConfig={rteToolbarConfig}
         />
       </div>
     </div>
